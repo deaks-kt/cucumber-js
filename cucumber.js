3ac9132f42b53a0c27a7b67d0233748ceb7ee8b0
charlierudolph <charles.rudolph@originate.com>
2016-03-27 22:25:11 -0700

    update

--- cucumber.js
+++ cucumber.js
@@ -3,5 +3,5 @@ common = '--strict --format rerun:@rerun.txt'
 module.exports = {
   build: common + ' --format progress',
   'default': common,
-  'ignore-es6': '--tags ~@es6'
+  'es5': '--tags ~@es6'
 };
